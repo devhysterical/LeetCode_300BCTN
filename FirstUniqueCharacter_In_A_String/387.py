4cb1138459701ee1d64e1b15bfcc345a464eddac
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2024-03-06 09:28:06 +0700

    Refactor firstUniqChar method in Solution class***
    ***Add comments and improve readability in firstUniqChar method***
    
    ***Update time and space complexity in firstUniqChar method

--- FirstUniqueCharacter_In_A_String/387.py
+++ FirstUniqueCharacter_In_A_String/387.py
@@ -1,12 +1,30 @@
+# link: https://leetcode.com/problems/first-unique-character-in-a-string/
 class Solution:
-  def firstUniqChar(self, s: str) -> int:
-    char_count = {}
+    def firstUniqChar(self, s: str) -> int:
+        """
+        Find the index of the first non-repeating character in a given string.
 
-    for char in s:
-      char_count[char] = char_count.get(char, 0) + 1
-    
-    for i, char in enumerate(s):
-      if char_count[char] == 1:
-        return i
-    
-    return -1
+        Given a string 's', this function calculates the index of the first non-repeating character.
+        If all characters are repeated, it returns -1.
+
+        Args:
+        - s (str): The input string.
+
+        Returns:
+        - int: The index of the first non-repeating character in 's', or -1 if no such character is found.
+        """
+        char_count = {}
+        # Count the occurrences of each character in the string
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+        # Iterate through the string to find the first non-repeating character
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
+                return i
+
+        # Return -1 if no non-repeating character is found
+        return -1
+
+
+# Time complexity: O(n)
+# Space complexity: O(1) because the size of the dictionary is at most 26
